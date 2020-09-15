2dcc499330486b587ed62b624cf94e2cf297484c
Ramon Villarreal Leal <ramon.villarreal.leal@gmail.com>
2020-09-15 10:35:05 -0500

    updating master strarting react

---
+++ app.js
@@ -0,0 +1,11 @@
+const title =React.createElement(
+    //type of element you want to create
+    //not a real h1 element its an object that describes a dom node
+    'h1',
+    // second argument is an object
+    {id: 'main-title', title: 'This is a title'},
+    // 3rd argument passed through is the content or children you are creating
+    'My first React element!'
+);
+
+console.log(title);
