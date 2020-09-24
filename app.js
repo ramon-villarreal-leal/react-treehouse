ed092ea573ab768fc29f5634b9940cca09f87423
Ramon Villarreal Leal <ramon.villarreal.leal@gmail.com>
2020-09-24 11:00:59 -0500

    updating React components

--- app.js
+++ app.js
@@ -1,48 +1,17 @@
-const title = 'My First React Element!';
-
-const desc = 'I just learned how to create a react node and render it to the DOM';
-
-const myTitleID = 'main-title';
-const name = 'Guil';
-//{} the Curly braces let JSX know you are writing JavaScript wooo
-// className is actually JSX's version of CSS's
-// const header = (
-//     <header>
-//         <h1 id={myTitleID}>{ name }'s First React Element!</h1>
-//         <p className={}>{desc}</p>
-//         <input value={10 * 20}/>
-//     </header>
-// )
-
-//function header, uppercase like Java
-const Header = () => {
+function Person(props) {
     return (
-        <header>
-            <h1>Scoreboard</h1>
-            <span className="stats">Players: 1</span>
-        </header>
-    );
-}
-
-const Player = () => {
-    return (
-        <div className="player">
-            <span className="player-name">
-                Guil
-            </span>
-            <div className="counter">
-                <button className="counter-action decrement"> - </button>
-                <span className="counter-score">35</span>
-                <button className="counter-action increment"> + </button>
-            </div>
+        <div className="person">
+            <h1>{props.name}</h1>
+            <p>Your Age: {props.age}</p>
         </div>
     );
 }
 
-//render html element
-ReactDOM.render(
-    <Player />,
-    document.getElementById('root')
-);
+ReactDOM.render(<Person name="Ramon" age="28" />,
+    document.querySelector('#p1'));
 
-console.log(title);
+ReactDOM.render(<Person name="Laura" age="26" />,
+    document.querySelector('#p2'));
+
+ReactDOM.render(<Person name="Laura" age="26" />,
+    document.querySelector('#p2'));
